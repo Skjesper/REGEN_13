4b5a7b28df990ef25cff75b33be51753171e2a20
Skjesper <jesperskeppstedt@gmail.com>
2024-11-21 15:55:11 +0100

    123

---
+++ Regen_13/JesperTestFil/JavaScript/meny-slider.js
@@ -0,0 +1,14 @@
+// Hämta element från DOM
+const menuButton = document.querySelector('.headerButtonMobile');
+const slideMenu = document.querySelector('.slide-menu');
+const closeButton = document.querySelector('.close-menu');
+
+// Öppna menyn när hamburgermenyn klickas
+menuButton.addEventListener('click', () => {
+  slideMenu.classList.add('open');
+});
+
+// Stäng menyn när stäng-knappen klickas
+closeButton.addEventListener('click', () => {
+  slideMenu.classList.remove('open');
+});
