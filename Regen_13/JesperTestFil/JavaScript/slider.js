e96068d72aa976ca618df05060cca616f6b79810
Skjesper <jesperskeppstedt@gmail.com>
2024-11-27 14:31:11 +0100

    forest

--- Regen_13/JesperTestFil/JavaScript/slider.js
+++ Regen_13/JesperTestFil/JavaScript/slider.js
@@ -8,9 +8,9 @@ function slide() {
     let slideValue = document.getElementById("slider").value;
 
     document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
-    
+
     document.querySelector(".divider").style.left = slideValue + "%";
-    
+
     console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
 }
 
