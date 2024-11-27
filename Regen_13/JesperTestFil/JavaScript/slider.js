ea99e010e06a9f44112e8d6c61d5a71aa98c7906
Skjesper <jesperskeppstedt@gmail.com>
2024-11-27 14:13:44 +0100

    Update slider.js

--- Regen_13/JesperTestFil/JavaScript/slider.js
+++ Regen_13/JesperTestFil/JavaScript/slider.js
@@ -1,10 +1,25 @@
-function slide(){
+// function slide(){
+//     let slideValue = document.getElementById("slider").value;
+//     document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+//     console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
+// }
+
+function slide() {
     let slideValue = document.getElementById("slider").value;
+
     document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+    
+    document.querySelector(".divider").style.left = slideValue + "%";
+    
     console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
 }
 
+window.onload = function() {
 
+    document.getElementById("slider").value = 50;
+
+    slide();
+};
 
 // Step-by-Step Explanation:
 
